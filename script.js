20647b9503924890a2b4e8124cbd8e5d9dc83ffc
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-05-30 20:54:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const isPowerOfTwo = (number) => {
-  //your code here
-};
-
-const num = parseInt(prompt("Enter a number"));
-alert(isPowerOfTwo(num));
-const isPowerOfTwo = (number) => {
-  // 1. 'number > 0' ensures we filter out 0 and negative numbers.
-  // 2. '(number & (number - 1)) === 0' checks the bitwise property of powers of two.
+function isPowerOfTwo(number) {
+  // Use a standard function declaration to prevent re-declaration errors in Cypress
   return number > 0 && (number & (number - 1)) === 0;
-};
+}
 
 const num = parseInt(prompt("Enter a number"));
 alert(isPowerOfTwo(num));
