f1bf3722f1cc64fe077a6dcd10f36fbc7fc4b9c1
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-05-30 20:48:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const isPowerOfTwo = (number) => {
 
 const num = parseInt(prompt("Enter a number"));
 alert(isPowerOfTwo(num));
+const isPowerOfTwo = (number) => {
+  // 1. 'number > 0' ensures we filter out 0 and negative numbers.
+  // 2. '(number & (number - 1)) === 0' checks the bitwise property of powers of two.
+  return number > 0 && (number & (number - 1)) === 0;
+};
+
+const num = parseInt(prompt("Enter a number"));
+alert(isPowerOfTwo(num));
